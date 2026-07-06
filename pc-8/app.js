ac28925589b24c667a51598406f62c3b33f50701
Subham Dhar <siddy934@gmail.com>
2026-07-06 21:19:32 +0530

    practice-7.1

--- pc-8/app.js
+++ pc-8/app.js
@@ -8,15 +8,27 @@
 
 // console.log(catMe());
 
+const express = require('express');
+const app = express();
 
-
-const express = require('express')
-const app = express()
+const catMe = require('cat-me');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World, Server is Working!!');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is about Page');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('This is contact Page');
+});
+
+app.get('/cat', (req, res) => {
+  res.send(catMe());
+});
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-})
+  console.log('Server is running on port 3000!');
+});
