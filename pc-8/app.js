b67d12e3ca774a7cd2207690a2f2f4c5dac34382
Subham Dhar <siddy934@gmail.com>
2026-07-06 22:30:34 +0530

    practice-7.2

--- pc-8/app.js
+++ pc-8/app.js
@@ -8,27 +8,49 @@
 
 // console.log(catMe());
 
-const express = require('express');
+// const express = require('express');
+// const app = express();
+
+// const catMe = require('cat-me');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World, Server is Working!!');
+// });
+
+// app.get('/about', (req, res) => {
+//   res.send('This is about Page');
+// });
+
+// app.get('/contact', (req, res) => {
+//   res.send('This is contact Page');
+// });
+
+// app.get('/cat', (req, res) => {
+//   res.send(catMe());
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000!');
+// });
+
+
+const express = require("express");
 const app = express();
 
-const catMe = require('cat-me');
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World, Server is Working!!');
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.send("Note created successfully on notes.");
 });
 
-app.get('/about', (req, res) => {
-  res.send('This is about Page');
+app.get("/notes", (req, res) => {
+  res.send(notes);
 });
 
-app.get('/contact', (req, res) => {
-  res.send('This is contact Page');
-});
-
-app.get('/cat', (req, res) => {
-  res.send(catMe());
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
