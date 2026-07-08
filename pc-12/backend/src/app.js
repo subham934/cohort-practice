961d19167825d6d2a1bddb7f4dde1668d068668b
Subham Dhar <siddy934@gmail.com>
2026-07-08 21:52:21 +0530

    practice-11.1

--- pc-12/backend/src/app.js
+++ pc-12/backend/src/app.js
@@ -60,10 +60,15 @@ app.delete('/api/notes/:id', async (req, res) => {
 
 app.patch('/api/notes/:id', async (req, res) => {
   const id = req.params.id;
-  const { description } = req.body;
-  await noteModel.findByIdAndUpdate(id, { description });
+  const { title, description } = req.body;
+  const note = await noteModel.findByIdAndUpdate(
+    id,
+    { title, description },
+    { new: true }
+  );
   res.status(200).json({
     message: 'Note updated successfully',
+    note,
   });
 });
 
