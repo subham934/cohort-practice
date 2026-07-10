eaeaea433cb1198578e10a635533dd7c1cbc4a22
Subham Dhar <siddy934@gmail.com>
2026-07-10 17:06:09 +0530

    practice-14

---
+++ pc-15/src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+const userModel = require('../models/user.model');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
+
+async function registerController(req, res){
+    
+}
