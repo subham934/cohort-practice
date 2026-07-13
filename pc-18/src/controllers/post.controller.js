e9714fd491654183a3506a7ba26bfa56959275b4
Subham Dhar <siddy934@gmail.com>
2026-07-13 21:17:22 +0530

    practice-17.1

--- pc-18/src/controllers/post.controller.js
+++ pc-18/src/controllers/post.controller.js
@@ -1,26 +1,57 @@
-const postModel = require("../models/post.model");
-const ImageKit = require("@imagekit/nodejs");
-const {toFile} = require("@imagekit/nodejs")
-
+const postModel = require('../models/post.model');
+const ImageKit = require('@imagekit/nodejs');
+const { toFile } = require('@imagekit/nodejs');
+const jwt = require('jsonwebtoken');
 
 const imagekit = new ImageKit({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
-})
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
 
 async function createPostController(req, res) {
   console.log(req.body, req.file);
 
+  // token check karo
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Token not provided, Unauthorized Access',
+    });
+  }
+
+  // verify token
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Token is not valid',
+    });
+  }
+
+  //    Image ko ImageKit pe upload karo
   const file = await imagekit.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer), "file"),
-    fileName: "Test",
-  })
+    file: await toFile(Buffer.from(req.file.buffer), 'file'),
+    fileName: 'Test',
+    folder: 'instaDoodle',
+  });
 
-  res.send(file)
+  //   Post ko database mein save karo
 
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id,
+  });
 
+  //   Response bhejo
+  res.status(201).json({
+    message: 'Post created Successfully',
+    post,
+  });
 }
 
 module.exports = {
   createPostController,
 };
-
